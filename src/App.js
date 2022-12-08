7cfda57bbc1b4e9945792497b3f8723987cb1a07
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-12-08 11:50:14 +0600

    simple node client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {
+      name,
+      email,
+    };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+        console.log(data);
+      })
+      .catch((err) => console.error(err));
+
+    form.reset();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Name" />
+          <br />
+          <input type="email" name="email" id="" placeholder="Email" />
+          <br />
+          <button type="submit">Add User</button>
+        </form>
+      </div>
+      <h2>User: {users.length}</h2>
+      <div>
+        {users.map((user, idx) => (
+          <p key={user._id}>
+            user name: {user.name}, email: {user.email}{" "}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
